baf2cd7079235fa26a1a7571c484b17375c55bf1
Onur Dilsiz <onurdilsiz0@gmail.com>
2024-12-20 15:25:23 +0300

    analysis for different prompts added

--- llmcompanion/success_rate_analysis.py
+++ llmcompanion/success_rate_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 # Reimport the dataset to ensure no duplicate columns
-results_df = pd.read_csv('results_all_cases.csv')
+results_df = pd.read_csv('detectionresults3.csv')
 
 # Calculate success rate for each model
 model_success_rate = results_df.groupby('Model')['Success'].mean() * 100
@@ -12,8 +12,8 @@ model_case_success_rate = results_df.groupby(['Model', 'Case'])['Success'].mean(
 case_success_rate = results_df.groupby('Case')['Success'].mean() * 100
 
 # Save results to CSV
-model_success_rate.to_csv("model_success.csv", header=["Success Rate"])
-case_success_rate.to_csv("case_success.csv", header=["Success Rate"])
-model_case_success_rate.to_csv("model_case_success.csv", header=["Success Rate"])
+model_success_rate.to_csv("specific_model_success.csv", header=["Success Rate"])
+case_success_rate.to_csv("specific_case_success.csv", header=["Success Rate"])
+model_case_success_rate.to_csv("specific_model_case_success.csv", header=["Success Rate"])
 
 print("\nSuccess rates have been calculated and saved to CSV files.")
